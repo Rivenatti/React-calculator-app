8f508ebbbb3fad958ee6ccd44379ac10b5c7172e
Marcin <Rivenatti@gmail.com>
2018-09-30 00:15:59 +0200

    repsonsive layout done

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -9,94 +9,66 @@ class Calculator extends Component {
         <div className="calculator__display">12345</div>
 
         {/* Functional keys row */}
-        <div className="calculator__function__keys--row">
+        <div className="calculator__function__keys--row first">
           <button className="calculator__key calculator__key--percentage">
             %
           </button>
           <button className="calculator__key calculator__key--square-root">
             &radic;
           </button>
-          <button className="calculator__key calculator__key--square-root">
+          <button className="calculator__key calculator__key--power">
             x&sup2;
           </button>
-          <button className="calculator__key calculator__key--square-root">
+          <button className="calculator__key calculator__key--one-division">
             1/x
           </button>
         </div>
         {/* Functional keys row */}
         <div className="calculator__function__keys--row">
-          <button className="calculator__key calculator__key--square-root">
-            CE
-          </button>
-          <button className="calculator__key calculator__key--square-root">
+          <button className="calculator__key calculator__key--clear span">
             C
           </button>
-          <button className="calculator__key calculator__key--square-root">
-            D
-          </button>
-          <button className="calculator__key calculator__key--square-root">
+
+          <button className="calculator__key calculator__key--delete">D</button>
+          <button className="calculator__key calculator__key--divide">
             &divide;
           </button>
         </div>
         {/* Functional keys row */}
         <div className="calculator__function__keys--row">
-          <button className="calculator__key calculator__key--square-root">
-            7
-          </button>
-          <button className="calculator__key calculator__key--square-root">
-            8
-          </button>
-          <button className="calculator__key calculator__key--square-root">
-            9
-          </button>
-          <button className="calculator__key calculator__key--square-root">
+          <button className="calculator__key calculator__key--seven">7</button>
+          <button className="calculator__key calculator__key--eight">8</button>
+          <button className="calculator__key calculator__key--nine">9</button>
+          <button className="calculator__key calculator__key--times">
             &times;
           </button>
         </div>
         {/* Functional keys row */}
         <div className="calculator__function__keys--row">
-          <button className="calculator__key calculator__key--square-root">
-            4
-          </button>
-          <button className="calculator__key calculator__key--square-root">
-            5
-          </button>
-          <button className="calculator__key calculator__key--square-root">
-            6
-          </button>
-          <button className="calculator__key calculator__key--square-root">
+          <button className="calculator__key calculator__key--four">4</button>
+          <button className="calculator__key calculator__key--five">5</button>
+          <button className="calculator__key calculator__key--six">6</button>
+          <button className="calculator__key calculator__key--minus">
             &minus;
           </button>
         </div>
         {/* Functional keys row */}
         <div className="calculator__function__keys--row">
-          <button className="calculator__key calculator__key--square-root">
-            1
-          </button>
-          <button className="calculator__key calculator__key--square-root">
-            2
-          </button>
-          <button className="calculator__key calculator__key--square-root">
-            3
-          </button>
-          <button className="calculator__key calculator__key--square-root">
+          <button className="calculator__key calculator__key--one">1</button>
+          <button className="calculator__key calculator__key--two">2</button>
+          <button className="calculator__key calculator__key--three">3</button>
+          <button className="calculator__key calculator__key--plus">
             &#x2b;
           </button>
         </div>
         {/* Functional keys row */}
         <div className="calculator__function__keys--row">
-          <button className="calculator__key calculator__key--square-root">
+          <button className="calculator__key calculator__key--plusminus">
             &plusmn;
           </button>
-          <button className="calculator__key calculator__key--square-root">
-            0
-          </button>
-          <button className="calculator__key calculator__key--square-root">
-            .
-          </button>
-          <button className="calculator__key calculator__key--square-root">
-            =
-          </button>
+          <button className="calculator__key calculator__key--zero">0</button>
+          <button className="calculator__key calculator__key--point">.</button>
+          <button className="calculator__key calculator__key--equal">=</button>
         </div>
       </div>
       // END OF CALCULATOR CLASS
